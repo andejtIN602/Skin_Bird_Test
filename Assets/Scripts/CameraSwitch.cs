97e3f1d25e5d437e96a87592ea077bc5c8cc5342
Jamie Anderson (1000023176) <ANDEJT1@student.op.ac.nz>
2020-03-16 14:49:23 +1300

    added bird asset

---
+++ Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public Camera PlayerView;
+    public Camera BirdView;
+    
+
+    void Start()
+    {
+        PlayerView.enabled = true;
+        BirdView.enabled = false;
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown("c"))
+        {
+            if (PlayerView.enabled == true)
+            {
+                PlayerView.enabled = false;
+                BirdView.enabled = true;
+            }
+            else
+            {
+                PlayerView.enabled = true;
+                BirdView.enabled = false;
+            }
+        }
+            
+    }
+}
